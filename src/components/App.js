9f57ff246f64f8b523b764eb325098e04b2323b6
Evan <eprice1223321@gmail.com>
2021-01-08 11:57:57 -0600

    ADD: abandoned the tutorial again because I wanted to just try stuff. I am now making a top bar using https://codeburst.io/how-to-create-a-navigation-bar-and-sidebar-using-react-348243ccd93 as a tutorial reference.

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,23 @@
 import React, { useState } from 'react';
 
 export default function App() {
-  const [count, setCount] = useState(0);
   return (
-    <div>
-      This is a sample stateful and server-side
-      rendered React application.
-      <br />
-      <br />
-      Here is a button that will track
-      how many times you click it:
-      <br />
-      <br />
-      <button onClick={() => setCount(count + 1)}>{count}</button>
+    <div class = "title_spot">
+    <h2>Welcome to My Website</h2>
+    <section>
+    <dl>
+        <dt>
+    <h3>
+        Useful Resources
+    </h3>
+        </dt>
+    <h5>
+        <dd><a href="https://github.com/Evan-Price-projects/website" target="_blank">Link to this Repository</a></dd>
+        <dd><a href="https://github.com/Evan-Price-projects/" target="_blank">GitHub Homepage</a></dd>
+        <dd><a href="https://www.linkedin.com/in/evan-price-57784385/" target="_blank">LinkedIn Homepage</a></dd>
+    </h5>
+    </dl>
+    </section>
     </div>
   );
 }
